606972fb6d99f191dcdcff31ab29214fe807cb2a
Максим Колтович <12345kolt@gmail.com>
2022-01-25 19:29:38 +0300

    toDo list

--- js/script.js
+++ js/script.js
@@ -5,18 +5,20 @@ const toDosWrapper = document.querySelector('.content-wrapper');
 let tasks;
 !localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'));
 
+let todoItemElems = [];
+
 function Task(description) {
     this.description = description;
     this.completed = false;
 }
 
 const createTemplate = (task, index) => {
-    return  `
-    <div className="todo-item">
-        <div className="description">${task.description}</div>
-        <div className="bottons">
-            <input className="btn-complete" type="checkbox"/>
-            <button className="btn-delete">Delete</button>
+    return `
+    <div class="todo-item ${task.completed ? 'checked' : ''}">
+        <div class="description">${task.description}</div>
+        <div class="buttons">
+            <input onclick="comleteTasks(${index})" class="btn-complete" type="checkbox" ${task.completed ? 'checked' : ''}/>
+            <button onclick="deleteTasks(${index})" class="btn-delete">Delete</button>
         </div>
      </div>
    `
@@ -27,18 +29,35 @@ const fillHtmlList = () => {
     if (tasks.length > 0) {
         tasks.forEach((item, index) => {
             toDosWrapper.innerHTML += createTemplate(item, index);
-        })
+        });
+        todoItemElems = document.querySelectorAll('.todo-item');
     }
 }
 fillHtmlList();
 
 const updateLocal = () => {
-    localStorage.setItem('tasks', JSON.stringify(tasks))
-
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
+
+const comleteTasks = index => {
+    tasks[index].completed = !tasks[index].completed;
+    if (tasks[index].completed) {
+        todoItemElems[index].classList.add('checked');
+    } else {
+        todoItemElems[index].classList.remove('checked');
+    }
+    updateLocal();
+    fillHtmlList();
+}
+
 addTascyBtn.addEventListener('click', () => {
     tasks.push(new Task(descTascyInput.value));
     console.log(tasks);
     updateLocal();
     fillHtmlList();
+    descTascyInput.value = '';
 })
+
+const deleteTasks = index => {
+
+}
